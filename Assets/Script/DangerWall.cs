536dbf049dfb6db60fcbb8ef8486dc1750d10523
gusyazero <gusyazero@gmail.com>
2016-03-02 13:09:59 +0900

    first import

---
+++ Assets/Script/DangerWall.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class DangerWall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision hit)
+    {
+        if (hit.gameObject.CompareTag("Player"))
+        {
+            //Application.LoadLevel(Application.loadedLevel);
+            //SceneManager.LoadScene("stage01");
+            SceneManager.LoadScene(Application.loadedLevel);
+        }
+    }
+}
---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10.0f;
+    
+    // Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void FixedUpdate ()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.AddForce(x * speed, 0, z * speed);
+    }
+}
